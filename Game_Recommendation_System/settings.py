34e00822c4aec1b956964b16535fb96897feea69
agherashyam2000 <agherashyamkumar2000@gmail.com>
2024-02-19 14:26:23 -0500

    Created requirements.txt file

--- Game_Recommendation_System/settings.py
+++ Game_Recommendation_System/settings.py
@@ -54,7 +54,8 @@ ROOT_URLCONF = "Game_Recommendation_System.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'templates']
+        ,
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [

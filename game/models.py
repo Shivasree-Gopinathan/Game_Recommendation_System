e2a053f0bb4e962f5534eefd073ff69fb279f875
agherashyam2000 <agherashyamkumar2000@gmail.com>
2024-02-25 17:14:47 -0500

    Created Scripts to add games in database. And created Platform Model and Updated Game model.

--- game/models.py
+++ game/models.py
@@ -1,14 +1,20 @@
 from django.db import models
 
+class Platform(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Game(models.Model):
     title = models.CharField(max_length=100)
     genre = models.CharField(max_length=50)
-    platform = models.CharField(max_length=20)
+    platforms = models.ManyToManyField(Platform)
     release_date = models.DateField()
     developer = models.CharField(max_length=100)
     publisher = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to='../images/', blank=False)
+    image = models.ImageField(upload_to='images/', blank=False)
     avg_rating = models.FloatField()
 
     def __str__(self):

2d6dfb2c56091028fa3c8f1e968b952a3e2cddeb
vinisha1100 <vinisharajasekar11@gmail.com>
2024-03-14 15:31:19 -0400

    initial login and signup code

--- Game_Recommendation_System/settings.py
+++ Game_Recommendation_System/settings.py
@@ -11,10 +11,16 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Define the base directory of your project
+DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Define the media URL and root directory
+MEDIA_URL = ''
+MEDIA_ROOT = os.path.join(DIR, '')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/

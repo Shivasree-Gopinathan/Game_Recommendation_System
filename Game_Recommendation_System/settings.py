ac450db43f6781b17925b620cf1a3aa9acba481e
agherashyam2000 <agherashyamkumar2000@gmail.com>
2024-03-14 17:12:15 -0400

    Fixed Login, Signup and Re-structured project

--- Game_Recommendation_System/settings.py
+++ Game_Recommendation_System/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+
+from django.conf.urls import static
+from django.contrib import staticfiles
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -123,6 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # The directory where collectstatic will gather static files for deployment
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Directory containing static files for your apps
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

52515ada8fe1e5bc2ed5f5725e07af58992f37bd
Ajay <shanmugam13ajay@gmail.com>
2024-03-21 02:40:42 -0400

    Added Explore page and navigations

--- Game_Recommendation_System/urls.py
+++ Game_Recommendation_System/urls.py
@@ -17,16 +17,19 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
+from homepage import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('home/',include('homepage.urls')),
     path('game/',include('game.urls')),
+    path('showgames/', views.showgames, name='showgames'),
     path('', include('user.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from game.models import Game
 from homepage.models import Genre
+from game.models import Game, Platform
+from datetime import datetime
+from functools import reduce
+from django.db.models import Q
 
 def home(request):
     games = Game.objects.all()
@@ -23,4 +26,33 @@ def genre(request):
 #     else:
 #         games = Game.objects.all()
 #     return render(request, 'game_search.html', {'games': games})
+def showgames(request):
+    games = Game.objects.all().distinct()
+    query = request.GET.get('q')
+    genre = request.GET.get('genre')
+    platform = request.GET.get('platform')
 
+    unique_genres = set()
+    for game in games:
+        unique_genres.update(game.genre.split(","))
+    unique_genres = sorted(list(unique_genres))
+
+    ld = datetime(2021, 1, 1)
+    lessdategames = Game.objects.filter(release_date__gte=ld)
+    pc_platform = Platform.objects.get(name='Windows PC')
+    pcgames = Game.objects.filter(platforms=pc_platform)
+    main_content = 1
+
+    if query or genre:
+        main_content = 0
+        games = games.filter(title__icontains=query).distinct()
+    if genre:
+        # Split the input genre string and filter games based on any matching genre
+        genres_list = [g.strip() for g in genre.split(",")]
+        genre_filter = reduce(lambda x, y: x | y, [Q(genre__icontains=g) for g in genres_list])
+        games = games.filter(genre_filter)
+    if platform:
+        games = games.filter(platform__icontains=platform)
+
+
+    return render(request, 'homepage/showgames.html', {'games': games, 'unique_genres': unique_genres, 'less_dategames': lessdategames, 'pcgames': pcgames, 'main_content': main_content})

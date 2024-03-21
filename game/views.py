849180e267531b2f5b965cacf2b717f91b5dc7f3
Galla Devendra <galla.devendra1508@gmail.com>
2024-03-21 17:55:53 -0400

    Update the changes for user game validation

--- game/views.py
+++ game/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 from .forms import GameReviewForm
 from .models import Game, GameReview
+from payment.models import Payment
 from django.shortcuts import get_object_or_404
 from django.views import View
 
@@ -12,10 +13,12 @@ from django.views import View
 
 def game_details(request, game_id):
     game = get_object_or_404(Game, pk=game_id)
+    user_has_paid = Payment.objects.filter(user=request.user, game_name=game.title).exists()
     game_review = GameReview.objects.filter(game=game)
     for review in game_review:
         review.stars = render_stars(review.rating)
-    return render(request, 'game/game_details.html', {'game': game, 'game_review': game_review})
+    return render(request, 'game/game_details.html',
+                  {'game': game, 'game_review': game_review, 'user_has_paid': user_has_paid})
 
 
 def render_stars(rating):
